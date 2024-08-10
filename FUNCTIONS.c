2a40bea3863631e57d669b675eb98d7fb1b570de
Giorgi Samkharadze <giorgi.samkharadze.21@gmail.com>
2024-08-10 19:14:44 -0400

    Create FUNCTIONS.c

---
+++ FUNCTIONS.c
@@ -0,0 +1,33 @@
+int ADD(int x, int y)
+{
+	return x + y;
+}
+int SUB(int x, int y)
+{
+	return x - y;
+}
+int MUL(int x, int y)
+{
+	return x * y;
+}
+int DEV(int x, int y)
+{
+
+	return x / y;
+}
+int FACT(int x)
+{
+	int k = 1;
+	int y = 1;
+	while (y != x + 1)
+	{
+
+		k = k * y++;
+
+
+
+
+	}
+
+	return k;
+}
